f140478b99edffcd0f3d5165f2398071daec2fa8
ALEK <Administrator@ALEK>
2020-08-14 21:06:22 +0700

    Crarte Model

--- Models/TodosContext.cs
+++ Models/TodosContext.cs
@@ -3,13 +3,38 @@
 
 namespace TodoAlek.Models
 {
-    public class TodosContext:DbContext
+    public partial class TodosContext:DbContext
     {
-        public TodosContext(DbContextOptions<TodosContext> options): 
+        public  TodosContext(DbContextOptions<TodosContext> options): 
             base(options)
         {
         }
 
         public DbSet<Todos> Todos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todos>(entity =>
+            {
+                entity.HasKey(e => e.Id)
+                    .HasName("PK__Products__B40CC6CDFEF2D15E");
+
+                entity.Property(e => e.TimeExpire).HasColumnType("dattime");
+
+                entity.Property(e => e.TitleJob).HasColumnType("varchar(max)");
+
+
+                entity.Property(e => e.DescriptionJob).HasColumnType("varchar(max)");
+                    
+                entity.Property(e => e.IsComplete).HasColumnType("tinyint"); 
+
+                entity.Property(e => e.PercentageComplete).HasColumnType("int");
+            });
+
+           
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
